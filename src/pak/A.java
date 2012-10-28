93b040562465463ab3849c806e3fb257f86a5157
Jordan Ell <jordan.ell7@gmail.com>
2012-10-28 15:10:20 -0700

    Added files A and B

---
+++ src/pak/A.java
@@ -0,0 +1,34 @@
+package pak;
+
+public class A
+{
+	private String myString;
+	private int myInt;
+	
+	public A(String myString, int myInt)
+	{
+		super();
+		this.myString = myString;
+		this.myInt = myInt;
+	}
+	
+	public String getMyString()
+	{
+		return myString;
+	}
+	
+	public void setMyString(String myString)
+	{
+		this.myString = myString;
+	}
+	
+	public int getMyInt()
+	{
+		return myInt;
+	}
+	
+	public void setMyInt(int myInt)
+	{
+		this.myInt = myInt;
+	}
+}
---
+++ src/pak/B.java
@@ -0,0 +1,38 @@
+package pak;
+
+public class B
+{
+	private A a;
+	private int myInt;
+	
+	public B(A a, int myInt)
+	{
+		super();
+		this.a = a;
+		this.myInt = myInt;
+	}
+	
+	public String getMyAString() {
+		return a.getMyString();
+	}
+
+	public A getA()
+	{
+		return a;
+	}
+
+	public void setA(A a)
+	{
+		this.a = a;
+	}
+
+	public int getMyInt()
+	{
+		return myInt;
+	}
+
+	public void setMyInt(int myInt)
+	{
+		this.myInt = myInt;
+	}
+}
