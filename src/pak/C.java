eca48c1f08a617038d6d06cf8eadb8c52ee24a8d
Jordan Ell <jordan.ell7@gmail.com>
2012-10-29 11:11:01 -0700

    Implemented file C.

--- src/pak/C.java
+++ src/pak/C.java
@@ -1,1 +1,33 @@
-// This is a test
+package pak;
+
+public class C {
+	private int myInt;
+	private B b;
+	
+	public C(int myInt, B b)
+	{
+		super();
+		this.myInt = myInt;
+		this.b = b;
+	}
+
+	public int getMyInt()
+	{
+		return myInt;
+	}
+
+	public void setMyInt(int myInt)
+	{
+		this.myInt = myInt;
+	}
+
+	public B getB()
+	{
+		return b;
+	}
+
+	public void setB(B b)
+	{
+		this.b = b;
+	}
+}
